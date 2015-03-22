b2debd3c6aa21f11979e92e827b42b779f6dcf1d
szalonyiwan <andrzej.pigon@gmail.com>
2015-03-22 14:21:09 +0100

    dodana biblioteka volley.jar do łaczenia się z serwerem.

---
+++ MateFinder/app/src/main/java/pl/mf/zpi/matefinder/app/AppConfig.java
@@ -0,0 +1,13 @@
+package pl.mf.zpi.matefinder.app;
+
+/**
+ * Created by root on 22.03.15.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://156.17.130.212/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://156.17.130.212/android_login_api/";
+}
