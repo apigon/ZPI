c84381f36e2080dd79ec7028e0933eb367929366
TomP147 <tomekk57@gmail.com>
2015-05-02 23:34:54 +0200

    Zaimplementowe dodawanie użytkownika do znajomych oraz umieszczenie danych znajomego w bazie sqlite.

---
+++ MateFinder/app/src/main/java/pl/mf/zpi/matefinder/AddFriendActivity.java
@@ -0,0 +1,187 @@
+package pl.mf.zpi.matefinder;
+
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pl.mf.zpi.matefinder.app.AppConfig;
+import pl.mf.zpi.matefinder.app.AppController;
+import pl.mf.zpi.matefinder.helper.SQLiteHandler;
+
+
+public class AddFriendActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final String TAG = "addFriend";
+    private ProgressDialog pDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button dodajDoZnajomych = (Button) findViewById(R.id.dodaj_do_znajomych);
+        dodajDoZnajomych.setOnClickListener(this);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_friend, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == android.R.id.home)
+        {
+            backToMain();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String login = ((EditText) findViewById(R.id.login)).getText().toString();
+        if (!login.equals(""))
+            addFriend(login);
+        else {
+            Toast.makeText(getApplicationContext(),
+                    "Proszę podać login użytkownika dodawanego do znajomych !", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+    private void addFriend(final String login){
+        // Tag used to cancel the request
+        String tag_string_req = "addUserToFriends_req";
+
+        pDialog.setMessage("Dodawanie...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppConfig.URL_REGISTER, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+                        SQLiteHandler db = new SQLiteHandler(getApplicationContext());
+                        JSONObject user = jObj.getJSONObject("user");
+                        String userID = user.getString("userID");
+                        String login = user.getString("login");
+                        String email = user.getString("email");
+                        String phone = user.getString("phone_number");
+                        String name = user.getString("name");
+                        String surname = user.getString("surname");
+                        String photo = user.getString("photo");
+                        String location = user.getString("location");
+                        // Inserting row in users table
+                        db.addFriend(userID, login, email, phone, name, surname, photo, location);
+                        // Launch login activity
+                        backToMain();
+                    } else {
+
+                        // Error occurred in registration. Get the error
+                        // message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Adding friend Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                SQLiteHandler db = new SQLiteHandler(getApplicationContext());
+                HashMap<String, String> user = db.getUserDetails();
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("tag", "addUserToFriends");
+                params.put("ownerID", user.get("userID"));
+                params.put("groupName", "Znajomi");
+                params.put("userLogin",login);
+
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void backToMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
