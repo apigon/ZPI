cc420c4f39f154d300a92250fd39a252110db88f
TomP147 <tomekk57@gmail.com>
2015-05-02 23:28:46 +0200

    Zaimplementowe dodawanie użytkownika do znajomych oraz umieszczenie danych znajomego w bazie sqlite.

--- MateFinder/app/src/main/java/pl/mf/zpi/matefinder/ZakladkaZnajomi.java
+++ MateFinder/app/src/main/java/pl/mf/zpi/matefinder/ZakladkaZnajomi.java
@@ -1,14 +1,22 @@
 package pl.mf.zpi.matefinder;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -18,7 +26,6 @@ import pl.mf.zpi.matefinder.helper.SQLiteHandler;
  * Created by root on 12.04.15.
  */
 public class ZakladkaZnajomi extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.zakladka_znajomi, container, false);
