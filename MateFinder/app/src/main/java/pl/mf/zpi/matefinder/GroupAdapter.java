cce59757ca38f0c0ead8b9a63758ceae16fbd7fb
szalonyiwan <andrzej.pigon@gmail.com>
2015-05-10 23:56:06 +0200

    popup menu dla grup

--- MateFinder/app/src/main/java/pl/mf/zpi/matefinder/GroupAdapter.java
+++ MateFinder/app/src/main/java/pl/mf/zpi/matefinder/GroupAdapter.java
@@ -2,11 +2,16 @@ package pl.mf.zpi.matefinder;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,13 +22,14 @@ import pl.mf.zpi.matefinder.helper.SQLiteHandler;
 /**
  * Created by root on 04.05.15.
  */
-public class GroupAdapter extends BaseAdapter {
+public class GroupAdapter extends BaseAdapter implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener, PopupMenu.OnMenuItemClickListener{
 
-    SQLiteHandler dbHandler;
-    ArrayList<Group> groups;
-    Context context;
+    private SQLiteHandler dbHandler;
+    private ArrayList<Group> groups;
+    private Context context;
+    private ListView listView;
 
-    public GroupAdapter(Context c){
+    public GroupAdapter(Context c, ListView list){
         dbHandler = new SQLiteHandler(c);
         List<HashMap<String, String>> groupsDB = dbHandler.getGroupsDetails();
         groups = new ArrayList<Group>();
@@ -32,6 +38,7 @@ public class GroupAdapter extends BaseAdapter {
             groups.add(group);
         }
         context=c;
+        listView = list;
     }
     @Override
     public int getCount() {
@@ -57,5 +64,25 @@ public class GroupAdapter extends BaseAdapter {
         Group tmp = groups.get(position);
         name.setText(tmp.getName());
         return row;
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        PopupMenu menu = new PopupMenu(context, listView.getChildAt(position));
+        menu.getMenuInflater().inflate(R.menu.group_popup_menu, menu.getMenu());
+        menu.setOnMenuItemClickListener(this);
+        menu.show();
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast t =Toast.makeText(context,"onClick", Toast.LENGTH_SHORT);
+        t.show();
     }
 }
