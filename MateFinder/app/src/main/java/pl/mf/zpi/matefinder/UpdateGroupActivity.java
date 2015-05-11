4eff6d7e7e6e6e17c8d63740a2dff72e8df4b222
szalonyiwan <andrzej.pigon@gmail.com>
2015-05-11 05:09:54 +0200

    edycja grupy po stronie serwera, nie testowane bo jakiś ciulowy program używa plików php na serwerze i nie mogę zapisać skryptów ;)

--- MateFinder/app/src/main/java/pl/mf/zpi/matefinder/UpdateGroupActivity.java
+++ MateFinder/app/src/main/java/pl/mf/zpi/matefinder/UpdateGroupActivity.java
@@ -1,17 +1,32 @@
 package pl.mf.zpi.matefinder;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.identity.intents.AddressConstants;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pl.mf.zpi.matefinder.app.AppConfig;
+import pl.mf.zpi.matefinder.app.AppController;
 import pl.mf.zpi.matefinder.helper.SQLiteHandler;
 
 
@@ -19,6 +34,9 @@ public class UpdateGroupActivity extends ActionBarActivity implements View.OnCli
 
     private Group group;
     private TextView name;
+    private ProgressDialog pDialog;
+    private SQLiteHandler db;
+    private static final String TAG = "updateGroup";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +57,11 @@ public class UpdateGroupActivity extends ActionBarActivity implements View.OnCli
 
         name.setText(group.getName());
         save.setOnClickListener(this);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+        db = new SQLiteHandler(this);
 
     }
 
@@ -64,11 +87,95 @@ public class UpdateGroupActivity extends ActionBarActivity implements View.OnCli
         SQLiteHandler db = new SQLiteHandler(this);
         String newName = name.getText().toString();
         if(!newName.equals(group.getName())) {
-            db.updateGroup(group.getID(), newName);
-            group.setName(newName);
+            updateGroup(newName);
         }
+        else{
+            Toast t = Toast.makeText(this, "Nic nie zmieniono!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+    private void updateGroup(final String nazwa){
+        // Tag used to cancel the request
+        //db = new SQLiteHandler(getApplicationContext());
+        String tag_string_req = "updateGroup_req";
+
+        pDialog.setMessage("Zapisywanie...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppConfig.URL_REGISTER, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+                    if (!error) {
+                        Toast.makeText(getApplicationContext(), "Zaktualizowano grupę!", Toast.LENGTH_LONG);
+                        db.updateGroup(group.getID(), group.getName());
+                        // Launch login activity
+                        backToMain();
+                    } else {
+
+                        // Error occurred in registration. Get the error
+                        // message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Adding group Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                SQLiteHandler db = new SQLiteHandler(getApplicationContext());
+                HashMap<String, String> user = db.getUserDetails();
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("tag", "updateGroup");
+                params.put("userID", user.get("userID"));
+                params.put("groupID", group.getID()+"");
+                params.put("groupName", nazwa);
+                params.put("oldName", group.getName());
+
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+    private void backToMain(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
+
 }
